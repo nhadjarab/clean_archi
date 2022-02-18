965c2c4f6a8941f03a86b5da01efee1accc257e9
Nassim HADJ-ARAB <nassim.hadjarab@gmail.com>
2022-02-18 21:41:19 +0100

    changed variable names and added a costants file

---
+++ Csharp/SalesReporter.Console/Constants.cs
@@ -0,0 +1,13 @@
+namespace SalesReporterKata;
+
+public class Constants
+{
+    public static string NUMBER_OF_SALES = "Number of sales";
+    public static string TOTAL_ITEMS_SOLD = "Total items sold";
+    public static string TOTAL_SALES_AMOUNT = "Total sales amount";
+    public static string AVERAGE_AMOUNT_PER_SALE = "Average amount/sale";
+    public static string AVERAGE_ITEM_PRICE = "Average item price";
+    public static string NUMBER_OF_CLIENTS = "Number of clients";
+    
+    
+}
---
+++ modifications.md
@@ -0,0 +1,16 @@
+# refacto 
+
+- Code difficile à lire car nom de variables non explicite => renommer les variables.
+- tout le code dans un seul fichier => mauvaise pratique.
+- Number of Clients n'est pas calculé.
+
+
+## 1. renommage varialbes 
+    - line1 : headerLine
+    - otherLines: dataLines
+    - number1: numberOfSales
+    - number2: totalItemsSold
+    - number3: totalSalesAmount
+    - number4: averageAmountPerSale
+    - number5: averageItemPrice
+
